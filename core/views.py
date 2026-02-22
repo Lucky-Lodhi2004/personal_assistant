ab3cde708960da9c5dd39599b524c4fdbb5c0ce4
Lucky Lodhi <luckysinghlodhi31@gmail.com>
2026-02-22 05:33:43 +0000

    added views, urls, and templates

--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def login(request):
+    return render(request, 'core/Login.html')
+
+def register(request):
+    return render(request, 'core/Register.html')
+
+def about(request):
+    return render(request, 'core/About.html')
+
+def dashboard(request):
+    return render(request, 'core/Dashboard.html')
